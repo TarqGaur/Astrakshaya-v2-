581d63c97db9bada72f58a9d71ffaba1adf9cc4f
TarqGaur <89157771+TarqGaur@users.noreply.github.com>
2025-07-24 16:04:32 +0530

    commit

---
+++ gotham/templates/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBsgaiVhiw1J1fU55tilho7fnTwHwxVasM",
+    authDomain: "gotham-signal.firebaseapp.com",
+    projectId: "gotham-signal",
+    storageBucket: "gotham-signal.firebasestorage.app",
+    messagingSenderId: "481579241989",
+    appId: "1:481579241989:web:c954fcf8ab98fe14e6826c",
+    measurementId: "G-2R4LS1JJJ1"
+};
